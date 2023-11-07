c4452039f9751ceb01e9e34f689eb1e5f4f43260
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2023-11-07 14:20:53 +0100

    Update CompileASM.sh

--- CompileASM.sh
+++ CompileASM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Don't give the extension. Filename: " name
+read -p "Filename (No Extension): " name
 nasm -f elf $name.asm
 ld -m elf_i386 -o main $name.o
 echo "Clearing File Object"

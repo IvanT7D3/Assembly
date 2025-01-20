245ff4f0c7bf45bb936097de1e65b597dee8a08b
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2025-01-20 06:30:10 -0600

    Update 5-Loop.asm

--- 5-Loop.asm
+++ 5-Loop.asm
@@ -4,7 +4,7 @@ global _start
 
 ;Functions should be above the entry point
 Print:
-		      ;The prolog is what happens at the beginning of a function. Its responsibility is to set up the stack frame of the called function.
+		      ;The prologue is what happens at the beginning of a function. Its responsibility is to set up the stack frame of the called function.
 	push ebp      ;Save the base pointer
 	mov ebp, esp  ;Set up a new base pointer
 
@@ -14,7 +14,7 @@ Print:
 	mov edx, stringL
 	int 0x80
 
-	;The epilog is what happens last in a function, and its purpose is to restore the stack frame of the calling function
+	;The epilogue is what happens last in a function, and its purpose is to restore the stack frame of the calling function
 	;We can do it using leave, or:
 	;mov esp ,ebp
 	;pop ebp
@@ -23,7 +23,7 @@ Print:
 
 _start:
 	mov ecx, 0x5 ;Set ECX to 5 (Loop | Counter register)
-	;In this case the loop will print the string Loops 5 times
+	;In this case the loop will print the string "Loops" 5 times
 	;Using the keyword loop in nasm, it will take what is inside of ECX, and start counting down from the number that is inside of ECX
 	;When we use loop, we must remember that when ECX finally hits 0, it will set the ZeroFlag
 	;This means that when it calls the loop again, it notices that the ZeroFlag is set, and skips the loop
@@ -37,7 +37,7 @@ LoopFunc:
 	popfd          ;Pop the flags register from the stack
 	popad          ;Pop all general-purpose registers from the stack
 	loop LoopFunc  ;Loop back to LoopFunc while ecx is not zero
-                       ;This is similar to jump not zero (jnz). Just keep in mind for this particular case we're using the loop keyword
+                       ;This is similar to jump not zero (jnz). Just keep in mind for this particular case, that we're using the loop keyword
 
 	;Return
 	mov eax, 0x1

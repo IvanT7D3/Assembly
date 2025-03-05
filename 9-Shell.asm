c6f73449c833b37a749d4f80837e486246ec493c
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2025-03-05 14:46:10 +0100

    Update 9-Shell.asm

--- 9-Shell.asm
+++ 9-Shell.asm
@@ -2,14 +2,14 @@ global _start
 
 _start:
 
-	mov eax, 0xB
-	mov ebx, shell
-	mov ecx, 0x0
-	mov edx, 0x0
-	int 0x80
+	mov eax, 0xB	;execve(
+	mov ebx, shell	;const char *filename
+	mov ecx, 0x0	;const char *const *argv
+	mov edx, 0x0	;const char *const *envp);
+	int 0x80	;execute system call
 
-	mov eax, 0x1
-	mov ebx, 0x0
-	int 0x80
+	mov eax, 0x1	;exit(
+	mov ebx, 0x0	;int error_code);
+	int 0x80	;execute system call
 
 	shell: db "/bin/sh"
